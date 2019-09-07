7cc7d3ae6c5640238d093502bd8521574bcfab2d
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-07 01:58:33 -0700

    making changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <div className="navbar">
-        <h1>Traveler Log</h1>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
+      <div className="App">
+        <Navigation />
       </div>
-    )
+    );
   }
 }
 
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Navigation.css';
 
 const Navigation = () => {
     return (
-
-    )
+       <div>
+           <ul id="nav">
+               <li><a href="#">Home</a></li>
+               <li><a href="#">About</a></li>
+               <li><a href="#">Destination</a></li>
+               <li><a href="#">More</a></li>
+           </ul>
+       </div>
+    );
 }
 
 export default Navigation;
